daa975c4918bc56021f265748a9a628bdbd49e7c
leeminy94 <leeminy94@we-gokorea.com>
2019-08-28 11:00:30 +0900

    닷홈 파일 넣고 업로드

---
+++ js/main.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+  
+  // variables
+  var $header_top = $('.header-top');
+  var $nav = $('nav');
+
+
+
+  // toggle menu 
+  $('.toggle-menu').on('click', function() {
+    $(this).parent().toggleClass('open-menu');
+  });
+  
+
+
+
+
+  // fullpage customization
+  
+function fullPage(){
+  $('#fullpage').fullpage({
+    sectionsColor: ['#B8AE9C', '#fff', '#334b3d', '#fff'],
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    css3: true,
+    controlArrows: false,
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    menu: '#menu',
+
+    afterLoad: function(anchorLink, index) {
+      $header_top.css('background', 'rgba(0, 47, 77, .3)');
+      $nav.css('background', 'rgba(0, 47, 77, .25)');
+      if (index == 5) {
+          $('#fp-nav').hide();
+        }
+    },
+
+    onLeave: function(index, nextIndex, direction) {
+      if(index == 5) {
+        $('#fp-nav').show();
+      }
+    },
+
+    afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
+      if(anchorLink == 'fifthSection' && slideIndex == 1) {
+        $.fn.fullpage.setAllowScrolling(false, 'up');
+        $header_top.css('background', 'transparent');
+        $nav.css('background', 'transparent');
+        $(this).css('background', '#374140');
+        $(this).find('h2').css('color', 'white');
+        $(this).find('h3').css('color', 'white');
+        $(this).find('p').css(
+          {
+            'color': '#DC3522',
+            'opacity': 1,
+            'transform': 'translateY(0)'
+          }
+        );
+      }
+    },
+
+    onSlideLeave: function( anchorLink, index, slideIndex, direction) {
+      if(anchorLink == 'fifthSection' && slideIndex == 1) {
+        $.fn.fullpage.setAllowScrolling(true, 'up');
+        $header_top.css('background', 'rgba(0, 47, 77, .3)');
+        $nav.css('background', 'rgba(0, 47, 77, .25)');
+      }
+    } 
+});	  // fullpage customization
+  }
+  fullPage();
+$(window).resize(function(){
+	if( $(this).width() < 480) {
+	$.fn.fullpage.destroy('all');
+	} 	
+});
+ $(window).trigger('resize');
+
+	$('.bxslider_1').bxSlider({
+  mode: 'fade',
+  captions: true
+});
+	
+	
+	
+	
+
+});
